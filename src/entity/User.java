f6ebfea871091bb2d9a62802bce824ebc3bd1183
stwig <S_kw888@yahoo.ca>
2023-10-30 10:07:21 -0400

    User Edit Entity

--- src/entity/User.java
+++ src/entity/User.java
@@ -3,14 +3,31 @@ package entity;
 public class User {
     private final String username;
     private final String password;
-    private RandomRecipe randomRecipe;
     private FavoriteRecipes favoriteRecipes;
-    private Recipe currentRandomRecipe;
     private Preference preference;
     User(String name, String password) {
         this.username = name;
         this.password = password;
     }
 
+    void setPreference(Preference preference){ //this is for initial user creation
+        this.preference = preference;
+    }
+
+    void addPreference(String type, String name){
+        //implement me
+    }
+
+    void removePreference(String type, String name){
+        //implement me
+    }
+
+    void addFavourite(Recipe recipe){
+        //implement me
+    }
+
+    void removeFavourite(Recipe recipe){
+        //implement me
+    }
 
 }
