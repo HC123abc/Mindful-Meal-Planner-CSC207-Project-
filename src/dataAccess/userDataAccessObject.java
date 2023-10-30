e3d3ed9b7631abf349b4f7934f87c633f9866748
stwig <S_kw888@yahoo.ca>
2023-10-30 10:30:33 -0400

    Signup interface file initialization for data access

---
+++ src/dataAccess/userDataAccessObject.java
@@ -0,0 +1,5 @@
+package dataAccess;
+import use_case.signUp.signUpDataAccessInterface;
+
+public class userDataAccessObject implements signUpDataAccessInterface {
+}
---
+++ src/use_case/signUp/signUpDataAccessInterface.java
@@ -0,0 +1,5 @@
+package use_case.signUp;
+
+public interface signUpDataAccessInterface {
+
+}
