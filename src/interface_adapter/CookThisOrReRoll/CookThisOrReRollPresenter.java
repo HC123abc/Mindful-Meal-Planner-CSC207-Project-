a03befe409d53be49e8063efced20c317184f880
HC123abc <126921906+HC123abc@users.noreply.github.com>
2023-11-13 09:47:28 -0500

    test

---
+++ src/interface_adapter/CookThisOrReRoll/CookThisOrReRollPresenter.java
@@ -0,0 +1,36 @@
+package interface_adapter.CookThisOrReRoll;
+
+import interface_adapter.ViewManagerModel;
+import use_case.generateRecipe.GenerateRecipeOutputBoundary;
+import use_case.generateRecipe.GenerateRecipeOutputData;
+
+public class CookThisOrReRollPresenter implements GenerateRecipeOutputBoundary {
+    private final CookThisOrReRollViewModel cookThisOrReRollViewModel;
+    private ViewManagerModel viewManagerModel;
+    public CookThisOrReRollPresenter(CookThisOrReRollViewModel cookThisOrReRollViewModel, ViewManagerModel viewManagerModel) {
+        this.cookThisOrReRollViewModel = cookThisOrReRollViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+    @Override
+    public void prepareSuccessView(GenerateRecipeOutputData recipeOutputData) {
+        CookThisOrReRollState cookThisOrReRollState = cookThisOrReRollViewModel.getState();
+        cookThisOrReRollState.setTitle(recipeOutputData.getTitle());
+        cookThisOrReRollState.setServings(recipeOutputData.getServings());
+        cookThisOrReRollState.setImage(recipeOutputData.getRecipeImageURL());
+        cookThisOrReRollState.setSummary(recipeOutputData.getSummary());
+        cookThisOrReRollState.setReadyInMinutes(recipeOutputData.getReadyInMinutes());
+        this.cookThisOrReRollViewModel.setState(cookThisOrReRollState);
+        cookThisOrReRollViewModel.firePropertyChanged();
+        viewManagerModel.setActiveView(cookThisOrReRollViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+//      main view model
+        CookThisOrReRollState cookThisOrReRollState = cookThisOrReRollViewModel.getState();
+        cookThisOrReRollState.setRecipeError(error);
+        cookThisOrReRollViewModel.firePropertyChanged();
+
+    }
+}
