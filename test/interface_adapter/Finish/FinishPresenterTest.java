61349e3c745ef5899cc6b14bd3e9baaedbef903f
HC123abc <126921906+HC123abc@users.noreply.github.com>
2023-11-18 16:10:22 -0500

    added finish presenter test

--- test/interface_adapter/Finish/FinishPresenterTest.java
+++ test/interface_adapter/Finish/FinishPresenterTest.java
@@ -1,4 +1,24 @@
-import static org.junit.jupiter.api.Assertions.*;
-class FinishPresenterTest {
-  
+package interface_adapter.Finish;
+
+import interface_adapter.ViewManagerModel;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FinishPresenterTest {
+
+    @Test
+    void prepareSuccessViewTest() {
+        // Mock ViewModel and ViewManagerModel
+        FinishViewModel mockViewModel = new FinishViewModel();
+        ViewManagerModel mockViewManagerModel = new ViewManagerModel();
+
+        // Creating FinishPresenter instance
+        FinishPresenter presenter = new FinishPresenter(mockViewManagerModel,mockViewModel);
+        presenter.prepareSuccessView();
+        // Verifying if viewManagerModel.setActiveView is equal to "Finish"
+        assertEquals("MainPage", mockViewManagerModel.getActiveView());
+
+    }
+
 }
