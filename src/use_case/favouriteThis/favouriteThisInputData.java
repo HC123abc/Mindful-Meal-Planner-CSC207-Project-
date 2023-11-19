e07d836e83fb5888a0c5af8f71c2ba997b31d86b
Ryan Yi <63077981+ryanzyi@users.noreply.github.com>
2023-11-19 18:42:18 -0500

    added getters for favouriteThis inputData object

--- src/use_case/favouriteThis/favouriteThisInputData.java
+++ src/use_case/favouriteThis/favouriteThisInputData.java
@@ -1,7 +1,7 @@
 package use_case.favouriteThis;
 
 public class favouriteThisInputData {
-    private String recipeError;
+    private String recipeError;  // TODO add implementation for error handling
     private String title;
     private String summary;
     private String image;
@@ -18,4 +18,34 @@ public class favouriteThisInputData {
                                   String id, boolean isFavourite) {
 
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSummary() { return summary; }
+
+    public String getImage() { return image; }
+
+    public String getServings() {
+        return servings;
+    }
+
+    public String getReadyInMinutes() {
+        return readyInMinutes;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public String getInstructions() {
+        return instruction;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public boolean getIsFavourite() { return isFavourite; }
 }
