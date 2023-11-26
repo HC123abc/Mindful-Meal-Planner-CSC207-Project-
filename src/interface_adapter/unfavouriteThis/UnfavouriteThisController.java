171a7f85815460f008222cb8d09b2fdf8b8f5353
Ryan Yi <63077981+ryanzyi@users.noreply.github.com>
2023-11-26 00:15:51 -0500

    implemented unfavouritethis use case controller

--- src/interface_adapter/unfavouriteThis/UnfavouriteThisController.java
+++ src/interface_adapter/unfavouriteThis/UnfavouriteThisController.java
@@ -1,4 +1,28 @@
 package interface_adapter.unfavouriteThis;
 
+import use_case.unfavouriteThis.UnfavouriteThisInputBoundary;
+import use_case.unfavouriteThis.UnfavouriteThisInputData;
+
 public class UnfavouriteThisController {
+    final UnfavouriteThisInputBoundary unfavouriteThisInteractor;
+
+    public UnfavouriteThisController(UnfavouriteThisInputBoundary unfavouriteThisInteractor) {
+        this.unfavouriteThisInteractor = unfavouriteThisInteractor;
+    }
+
+    public void execute(String title, String summary, String recipeImageURL,
+                               String servings, String readyInMinutes, String extendedIngredients,
+                               String extendedInstructions, String id) {
+
+            UnfavouriteThisInputData unfavouriteThisInputData = new UnfavouriteThisInputData(title,
+                                                                            summary,
+                                                                            recipeImageURL,
+                                                                            servings,
+                                                                            readyInMinutes,
+                                                                            extendedIngredients,
+                                                                            extendedInstructions,
+                                                                            id);
+
+            unfavouriteThisInteractor.execute(unfavouriteThisInputData);
+    }
 }
