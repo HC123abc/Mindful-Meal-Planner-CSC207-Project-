f98d39691706a53debd534e22088f61da7886d00
Ryan Yi <63077981+ryanzyi@users.noreply.github.com>
2023-11-26 00:20:37 -0500

    completed the output boundary and output data classes

--- src/use_case/unfavouriteThis/UnfavouriteThisOutputData.java
+++ src/use_case/unfavouriteThis/UnfavouriteThisOutputData.java
@@ -1,4 +1,15 @@
 package use_case.unfavouriteThis;
 
+import java.util.Map;
+
 public class UnfavouriteThisOutputData {
+    private Map<String, Map<String, String>> recipeOutput;
+
+    public UnfavouriteThisOutputData(Map<String, Map<String, String>> recipeOutput) {
+        this.recipeOutput = recipeOutput;
+    }
+
+    public Map<String, Map<String, String>> getRecipeOutput() {
+        return recipeOutput;
+    }
 }
