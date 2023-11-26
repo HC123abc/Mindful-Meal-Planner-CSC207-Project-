ba41374b288d17e0308e3c60541d5e69ae7a9300
Ryan Yi <63077981+ryanzyi@users.noreply.github.com>
2023-11-26 00:21:45 -0500

    updated favourites view to support unfavourite this use case

--- src/view/FavView.java
+++ src/view/FavView.java
@@ -4,6 +4,7 @@ import interface_adapter.CookThis.CookThisController;
 import interface_adapter.FavView.FavViewState;
 import interface_adapter.FavView.FavViewViewModel;
 import interface_adapter.Finish.FinishController;
+import interface_adapter.unfavouriteThis.UnfavouriteThisController;
 
 import javax.swing.*;
 import java.awt.*;
@@ -17,12 +18,14 @@ public class FavView extends JPanel implements PropertyChangeListener {
     public String viewName = "FavView";
     private FavViewViewModel favViewViewModel;
     private FinishController finishController;
+    private UnfavouriteThisController unfavouriteThisController;
     private CookThisController cookThisController;
 
-    public FavView(FavViewViewModel favViewViewModel, FinishController finishController, CookThisController cookThisController) {
+    public FavView(FavViewViewModel favViewViewModel, FinishController finishController, CookThisController cookThisController, UnfavouriteThisController unfavouriteThisController) {
         this.favViewViewModel = favViewViewModel;
         this.finishController = finishController;
         this.cookThisController = cookThisController;
+        this.unfavouriteThisController = unfavouriteThisController;
         this.favViewViewModel.addPropertyChangeListener(this);
 
         setLayout(new BorderLayout());
@@ -58,7 +61,7 @@ public class FavView extends JPanel implements PropertyChangeListener {
             String cardImageURL = entry.getKey();
             Map<String, String> recipeDetails = entry.getValue();
 
-            RecipeCard recipeCard = new RecipeCard(recipeDetails, cardImageURL, this.cookThisController);
+            RecipeCard recipeCard = new RecipeCard(recipeDetails, cardImageURL, this.cookThisController, this.unfavouriteThisController);
             recipePanel.add(recipeCard);
         }
         recipePanel.revalidate();
@@ -76,12 +79,18 @@ public class FavView extends JPanel implements PropertyChangeListener {
 
     private static class RecipeCard extends JPanel {
         private Map<String, String> recipeDetails;
+        private String cardImageURL;
         private ImageIcon image;
         private CookThisController cookThisController;
+        private UnfavouriteThisController unfavouriteThisContoller;
 
-        public RecipeCard(Map<String, String> recipeDetails, String cardImageURL, CookThisController cookThisController) {
+        public RecipeCard(Map<String, String> recipeDetails, String cardImageURL, CookThisController cookThisController, UnfavouriteThisController unfavouriteThisController) {
             this.recipeDetails = recipeDetails;
+            this.unfavouriteThisContoller = unfavouriteThisController;
             this.cookThisController = cookThisController;
+
+            this.cardImageURL = cardImageURL;
+
 
             setLayout(new BorderLayout());
             String title = recipeDetails.get("title");
@@ -120,6 +129,18 @@ public class FavView extends JPanel implements PropertyChangeListener {
             JButton cookThisBtn = new JButton("Cook This");
 
             removeFromFavoritesBtn.addActionListener(e -> {
+                String recipeTitle = recipeDetails.get("title");
+                String summary = recipeDetails.get("summary");
+                String recipeImageURL = recipeDetails.get("recipeImageURL");
+                String servings = recipeDetails.get("servings");
+                String readyInMinutes = recipeDetails.get("readyInMinutes");
+                String extendedIngredients = recipeDetails.get("extendedIngredients");
+                String extendedInstructions = recipeDetails.get("extendedInstructions");
+                String id = recipeDetails.get("id");
+
+                unfavouriteThisController.execute(recipeTitle, summary, recipeImageURL, servings,
+                        readyInMinutes, extendedIngredients, extendedInstructions, id);
+
                 System.out.println("Removing from favorites: " + recipeDetails.get("title"));
             });
 
