c64fa13745aa19957fc75633395a02581ae8e8f7
Ryan Yi <63077981+ryanzyi@users.noreply.github.com>
2023-11-26 00:19:56 -0500

    implemented the presenter for unfavourite this

--- src/interface_adapter/unfavouriteThis/UnfavouriteThisPresenter.java
+++ src/interface_adapter/unfavouriteThis/UnfavouriteThisPresenter.java
@@ -1,4 +1,41 @@
 package interface_adapter.unfavouriteThis;
 
-public class UnfavouriteThisPresenter {
+import interface_adapter.FavView.FavViewState;
+import interface_adapter.FavView.FavViewViewModel;
+import interface_adapter.ViewManagerModel;
+
+import use_case.unfavouriteThis.UnfavouriteThisOutputBoundary;
+import use_case.unfavouriteThis.UnfavouriteThisOutputData;
+
+import java.util.Map;
+
+public class UnfavouriteThisPresenter implements UnfavouriteThisOutputBoundary {
+    private final FavViewViewModel favViewViewModel;
+    private ViewManagerModel viewManagerModel;
+    public UnfavouriteThisPresenter(FavViewViewModel favViewViewModel, ViewManagerModel viewManagerModel) {
+        this.favViewViewModel = favViewViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+    @Override
+    public void prepareSuccessView(UnfavouriteThisOutputData unfavouriteThisOutputData) {
+        FavViewState favViewState = favViewViewModel.getState();
+
+        Map<String, Map<String, String>> recipes = unfavouriteThisOutputData.getRecipeOutput();
+        favViewState.setRecipeWithCardImage(recipes);
+
+        this.favViewViewModel.setState(favViewState);
+
+        favViewViewModel.firePropertyChanged();
+        viewManagerModel.setActiveView(favViewViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+    }
+
+//    @Override
+//    public void prepareFailView(String error) {
+//        FavViewState favViewState = favViewViewModel.getState();
+//        favViewState.setRecipeError(error);
+//        favViewViewModel.firePropertyChanged();
+//
+//        viewManagerModel.setActiveView();
+//    }
 }
